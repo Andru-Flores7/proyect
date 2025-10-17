c9be1aa2682ebb8fb8a10cb35b277b5f3b4d1588
AndrewFlowe-rs <arnaldomahone@gmail.com>
2025-10-17 20:53:05 -0300

    Base interface for all system Services

---
+++ domain/src/utils/types/Service.ts
@@ -0,0 +1,11 @@
+import type { Entity } from "./entity";
+
+
+export interface Service<T extends Entity> {
+    findAll(): Promise<T[]>;
+    findById(id: string): Promise<T | null>;
+    create (data:Omit<T, keyof Entity>): Promise<T>;
+    update (id:string, data:Partial<T>): Promise<T>;
+    delete (id:string): Promise<void>;
+    
+}
