5e78f4d47da9bebeeac8f0722552488b9227e98e
AndrewFlowe-rs <arnaldomahone@gmail.com>
2025-10-17 19:05:17 -0300

    add entiti category finish

--- domain/src/entities/Category.ts
+++ domain/src/entities/Category.ts
@@ -6,3 +6,49 @@ export interface ICategory extends Entity {
   description: string;
 }
 
+export class Category implements ICategory {
+  id: string;
+  name: string;
+  description: string;
+   createdAt: Date;
+  updatedAt?: Date;
+
+
+ constructor(
+    id: string,
+    name: string,
+    description: string,
+     createdAt?: Date,
+  updatedAt?: Date
+ 
+  ) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.createdAt = createdAt ?? new Date();
+  this.updatedAt = updatedAt;
+
+    this.validate();
+  }
+
+  private validate() :void {
+    if (!this.id?.trim()) {
+    throw new Error("La categoría debe tener un ID");
+  }
+    if (!this.name?.trim()) {
+      throw new Error("La categoría debe tener un nombre");
+    }
+     if (!this.description?.trim()) {
+    throw new Error("La categoría debe tener una descripción");
+  }
+  }
+
+  rename(newName: string) :void {
+    if (!newName?.trim()) throw new Error("El nombre no puede estar vacío");
+    this.name = newName;
+      this.updatedAt = new Date();
+
+  }
+
+}
+
