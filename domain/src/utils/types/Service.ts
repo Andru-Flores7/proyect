de4abccafb78a02bf38706aebe444fb5f92b4ff5
AndrewFlowe-rs <arnaldomahone@gmail.com>
2025-10-18 17:01:01 -0300

    add service interface

--- domain/src/utils/types/Service.ts
+++ domain/src/utils/types/Service.ts
@@ -1,11 +1,10 @@
 import type { Entity } from "./entity";
 
-
 export interface Service<T extends Entity> {
-    findAll(): Promise<T[]>;
-    findById(id: string): Promise<T | null>;
-    create (data:Omit<T, keyof Entity>): Promise<T>;
-    update (id:string, data:Partial<T>): Promise<T>;
-    delete (id:string): Promise<void>;
-    
+  findAll(): Promise<T[]>;
+  findById(id: string): Promise<T | null>;
+  create(data: Omit<T, keyof Entity>): Promise<T>;
+  update(id: string, data: Partial<T>): Promise<T>;
+  save(item: T): Promise<void>;
+  delete(id: string): Promise<void>;
 }
