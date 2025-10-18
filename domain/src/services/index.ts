c8ed7f2779fd3be7d070bdc0169722549a067d1f
AndrewFlowe-rs <arnaldomahone@gmail.com>
2025-10-18 17:13:27 -0300

    exports

---
+++ domain/src/services/index.ts
@@ -0,0 +1,10 @@
+// src/domain/services/index.ts
+export * from "./UserService.js";
+export * from "./BookService.js";
+export * from "./LoanService.js";
+export * from "./CategoryService.js";
+export * from "./UserService.js";
+export * from "./moks/MockUserService.js";
+export * from "./moks/MockBookService.js";
+export * from "./moks/MockLoanService.js";
+export * from "./moks/MockCategoryService.js";
